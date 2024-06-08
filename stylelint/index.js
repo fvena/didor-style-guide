f38037696ce26f9991bca76fd9b4f3613ed1915e
Francisco Vena <fvena32@gmail.com>
2024-06-08 11:37:34 +0200

    feat(stylelint): add stylelint config

--- stylelint/index.js
+++ stylelint/index.js
@@ -1,1 +1,17 @@
-module.exports = {};
+module.exports = {
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-recommended-vue/scss',
+    'stylelint-config-recess-order'
+  ],
+  rules: {
+    'no-descending-specificity': [true, { severity: 'warning' }],
+    'no-empty-source': [true, { severity: 'warning' }],
+    'selector-pseudo-class-no-unknown': [
+      true,
+      { ignorePseudoClasses: ['deep', 'global', 'slotted'] }
+    ],
+    'scss/comment-no-empty': [true, { severity: 'warning' }]
+  }
+}
